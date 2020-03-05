00e44fd66f356b4b30d85a3e2d6b20b32fe8c951
Andrew Ellis <a.w.ellis@gmail.com>
2020-03-05 19:56:48 +0100

    Add NonDecisionTime type definitions

--- src/ndttypes.jl
+++ src/ndttypes.jl
@@ -1,5 +1,15 @@
 abstract type AbstractNDT end
-getdt(drift::AbstractNDT) = drift.Δt
 
-struct NDT
+@with_kw struct NonDecisionTime{T<:Real} <: AbstractNDT
+    # ndt::Tuple{Float64, Float64};
+    lower::T
+    upper::T
+    function NonDecisionTime{T}(lower::T, upper::T) where T<:Real
+        @assert 0 < lower < upper
+        new{T}(lower, upper)
+    end
 end
+
+NonDecisionTime(lower::Int64, upper::Int64) = NonDecisionTime(convert(Float64, lower),
+                                                              convert(Float64, upper))
+NonDecisionTime(lower::Real, upper::Real) = NonDecisionTime(promote(lower, upper)...)
